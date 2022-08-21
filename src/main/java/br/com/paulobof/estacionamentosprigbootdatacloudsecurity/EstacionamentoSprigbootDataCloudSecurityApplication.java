8e41f465b66c8b994198f33180d0ff56a4083cac
Paulo Bof <paulobof@gmail.com>
2022-08-21 20:29:14 -0300

    Configuração Inicial

---
+++ src/main/java/br/com/paulobof/estacionamentosprigbootdatacloudsecurity/EstacionamentoSprigbootDataCloudSecurityApplication.java
@@ -0,0 +1,13 @@
+package br.com.paulobof.estacionamentosprigbootdatacloudsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstacionamentoSprigbootDataCloudSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EstacionamentoSprigbootDataCloudSecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/paulobof/estacionamentosprigbootdatacloudsecurity/EstacionamentoSprigbootDataCloudSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.paulobof.estacionamentosprigbootdatacloudsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstacionamentoSprigbootDataCloudSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
